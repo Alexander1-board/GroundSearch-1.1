3e944f5b61ed9de17969f19aa0cd0bd61b5ce605
Alexander McLennan <alexandermclennan@Alexanders-MacBook-Pro.local>
2025-08-02 15:17:08 +1000

    chore: fix setup

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    ignores: ['node_modules'],
+    rules: {
+      // add your rules here
+    },
+  },
+];
